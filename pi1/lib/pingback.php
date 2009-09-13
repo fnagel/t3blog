93a7cf4f90ac04c36c67861ff672c877afdd005d
mkrause <mkrause@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-13 20:41:17 +0000

    Review: #4608 - error_reporting settings overwritten in pi1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3blog/trunk@24407 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/lib/pingback.php
+++ pi1/lib/pingback.php
@@ -4,35 +4,35 @@ require_once('../../lib/xmlrpc-2.2/lib/xmlrpc.inc');
 require_once('../../lib/xmlrpc-2.2/lib/xmlrpcs.inc');
 
 /**
- * Starts pingback-process 
+ * Starts pingback-process
  *
  * @param 	object	$m
  * @return 	string	Confirmation or error message
 */
 function pbprocess($m) {
-        global $xmlrpcerruser;
+		global $xmlrpcerruser;
 
-        $x1 		= $m->getParam(0);
-        $x2 		= $m->getParam(1);
-        $source 	= $x1->scalarval(); # their article
-        $dest 		= $x2->scalarval(); # your article
+		$x1 		= $m->getParam(0);
+		$x2 		= $m->getParam(1);
+		$source 	= $x1->scalarval(); # their article
+		$dest 		= $x2->scalarval(); # your article
 
 
-        //USE CLI MODE?
-        global $TYPO3_CONF_VARS;
-		error_reporting (E_ALL ^ E_NOTICE);
+			//USE CLI MODE?
+		global $TYPO3_CONF_VARS;
+
 		if ($_SERVER['PHP_SELF']) {
 			if (!defined('PATH_thisScript')) define('PATH_thisScript',str_replace('//','/', str_replace('\\','/', $_SERVER['PHP_SELF'])));
 		} else {
 			if (!defined('PATH_thisScript')) define('PATH_thisScript',str_replace('//','/', str_replace('\\','/', $_ENV['_'])));
 		}
-		
+
 		if (!defined('PATH_site')) define('PATH_site', dirname(dirname(dirname(dirname(dirname(dirname(PATH_thisScript)))))).'/');
 		if (!defined('PATH_t3lib')) if (!defined('PATH_t3lib')) define('PATH_t3lib', PATH_site.'t3lib/');
-		
+
 		define('PATH_typo3conf', PATH_site.'typo3conf/');
 		define('TYPO3_mainDir', 'typo3/');
-		
+
 		if (!defined('PATH_typo3')) define('PATH_typo3', PATH_site.TYPO3_mainDir);
 		if (!defined('PATH_tslib')) {
 			if (@is_dir(PATH_site.'typo3/sysext/cms/tslib/')) {
@@ -41,74 +41,74 @@ function pbprocess($m) {
 				define('PATH_tslib', PATH_site.'tslib/');
 			}
 		}
-		
+
 		define('TYPO3_OS', stristr(PHP_OS,'win')&&!stristr(PHP_OS,'darwin')?'WIN':'');
 		define('TYPO3_MODE', 'BE');
-		
+
 		require_once(PATH_t3lib.'class.t3lib_div.php');
 		require_once(PATH_t3lib.'class.t3lib_extmgm.php');
 		require_once(PATH_t3lib.'config_default.php');
 		require_once(PATH_typo3conf.'localconf.php');
-		
+
 		require_once(PATH_t3lib.'class.t3lib_userauth.php');
 		require_once(PATH_t3lib.'class.t3lib_userauthgroup.php');
 		require_once(PATH_t3lib.'class.t3lib_beuserauth.php');
 		require_once(PATH_t3lib.'class.t3lib_htmlmail.php');
-		
+
 		// Connect to the database
 		require_once(PATH_t3lib.'class.t3lib_db.php');
 		$TYPO3_DB 	= t3lib_div::makeInstance('t3lib_DB');
 		$result 	= $TYPO3_DB->sql_pconnect(TYPO3_db_host, TYPO3_db_username, TYPO3_db_password);
-		$nodb 		= false; 
+		$nodb 		= false;
 		if(!$result){
 			$nodb = true ; //die("oh shi, no connection".TYPO3_db_host);
 		}
-		
-		
-		$TYPO3_DB->sql_select_db(TYPO3_db);		 
-		
-        if (url_exists($source)) { # source uri does not exist
-	        return new xmlrpcresp(0, 16, "Source uri does not exist");
-        }
-        if (url_got_link($source,$dest)) { # source uri does not have a link to target uri
+
+
+		$TYPO3_DB->sql_select_db(TYPO3_db);
+
+				if (url_exists($source)) { # source uri does not exist
+					return new xmlrpcresp(0, 16, "Source uri does not exist");
+				}
+				if (url_got_link($source,$dest)) { # source uri does not have a link to target uri
 			return new xmlrpcresp(0, 17, "Source uri does have link to target uri");
 		}
 
-        if (url_exists($dest)) { # target uri does not exist
+				if (url_exists($dest)) { # target uri does not exist
 			return new xmlrpcresp(0, 32, "Target uri does not exist");
 		}
-		
+
 		/*
 		check with allow_pings
-		        if (..) { # target uri cannot be used as target
-		        	return new xmlrpcresp(0, 33, "Target uri cannot be used as target");
+						if (..) { # target uri cannot be used as target
+							return new xmlrpcresp(0, 33, "Target uri cannot be used as target");
 				}
-		*/	
-		
+		*/
+
 		$res_exits = $TYPO3_DB->exec_SELECTquery(
 			'uid',
 			'tx_t3blog_pingback',
 			'url == '.$source.'  AND deleted = 0 AND hidden = 0' //for testing, sends it now
 		);
-		
-        if ($GLOBALS['TYPO3_DB']->sql_fetch_assoc($res_exits)) { # Pingback already registered
+
+				if ($GLOBALS['TYPO3_DB']->sql_fetch_assoc($res_exits)) { # Pingback already registered
 			return new xmlrpcresp(0, 48, "Target uri cannot be used as target");
 		}
 
-        if (1==2) { # Access denied
-	        return new xmlrpcresp(0, 49, "Access denied");
+				if (1==2) { # Access denied
+					return new xmlrpcresp(0, 49, "Access denied");
 		}
 
-        if (1==2) { # Could not communicate with upstream server or got error
-        	return new xmlrpcresp(0, 50, "Problem with upstream server");
+				if (1==2) { # Could not communicate with upstream server or got error
+					return new xmlrpcresp(0, 50, "Problem with upstream server");
 		}
 
-        if ($nodb) { # Generic fault code if not applicable to above
-        	return new xmlrpcresp(0, 50, "Unkown error");
+				if ($nodb) { # Generic fault code if not applicable to above
+					return new xmlrpcresp(0, 50, "Unkown error");
 		}
-		
+
 		$title = getTitleByUrl($source);
-				
+
 		$GLOBALS['TYPO3_DB']->exec_INSERTquery(
 			'tx_t3blog_pingback',	//TABLE
 			array(
@@ -120,10 +120,10 @@ function pbprocess($m) {
 				'url'=>$source,
 				'text'=>$text, //TODO -> write the url
 				'date'=>time()
-			)	//values array			
+			)	//values array
 		);
 
-        return new xmlrpcresp(new xmlrpcval("Pingback registered. Have a nice Day.", "string"));
+				return new xmlrpcresp(new xmlrpcval("Pingback registered. Have a nice Day.", "string"));
 }
 
 
@@ -133,23 +133,23 @@ function pbprocess($m) {
  * @param 	string 	$strURL: url-address
  * @return 	boolean	true if existing
 */
-function url_exists($strURL) { 
-    $resURL = curl_init(); 
-    curl_setopt($resURL, CURLOPT_URL, $strURL); 
-    curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1); 
-    curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback'); 
-    curl_setopt($resURL, CURLOPT_FAILONERROR, 1); 
+function url_exists($strURL) {
+		$resURL = curl_init();
+		curl_setopt($resURL, CURLOPT_URL, $strURL);
+		curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback');
+		curl_setopt($resURL, CURLOPT_FAILONERROR, 1);
 
-    curl_exec ($resURL); 
+		curl_exec ($resURL);
 
-    $intReturnCode = curl_getinfo($resURL, CURLINFO_HTTP_CODE); 
-    curl_close ($resURL); 
+		$intReturnCode = curl_getinfo($resURL, CURLINFO_HTTP_CODE);
+		curl_close ($resURL);
 
-    if ($intReturnCode != 200 && $intReturnCode != 302 && $intReturnCode != 304) { 
-       return false; 
-    }Else{ 
-        return true ; 
-    } 
+		if ($intReturnCode != 200 && $intReturnCode != 302 && $intReturnCode != 304) {
+			return false;
+		}Else{
+				return true ;
+		}
 }
 
 /**
@@ -159,22 +159,22 @@ function url_exists($strURL) {
  * @param	string	$dest: your article
  * @return 	boolean	true if valid
 */
-function url_got_link($strURL,$dest) { 
-    $resURL = curl_init(); 
-    curl_setopt($resURL, CURLOPT_URL, $strURL); 
-    curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1); 
-    curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback'); 
-    curl_setopt($resURL, CURLOPT_FAILONERROR, 1); 
+function url_got_link($strURL,$dest) {
+		$resURL = curl_init();
+		curl_setopt($resURL, CURLOPT_URL, $strURL);
+		curl_setopt($resURL, CURLOPT_BINARYTRANSFER, 1);
+		curl_setopt($resURL, CURLOPT_HEADERFUNCTION, 'curlHeaderCallback');
+		curl_setopt($resURL, CURLOPT_FAILONERROR, 1);
 
-    $content = curl_exec ($resURL); 
+		$content = curl_exec ($resURL);
 
-    curl_close ($resURL); 
+		curl_close ($resURL);
 
-    if(stripos($content,$dest)){
-    	return true;	
-    }else{
-    	return false; 
-    }     
+		if(stripos($content,$dest)){
+			return true;
+		}else{
+			return false;
+		}
 }
 
 
@@ -186,20 +186,20 @@ function url_got_link($strURL,$dest) {
 */
 function getTitleByUrl($sURL) {
 	$clSession = curl_init();
-	$iTimeout = 30; 
-	
-	curl_setopt ($clSession, CURLOPT_URL, $sURL); 
+	$iTimeout = 30;
+
+	curl_setopt ($clSession, CURLOPT_URL, $sURL);
 	curl_setopt ($clSession, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt ($clSession, CURLOPT_CONNECTTIMEOUT, $iTimeout);
 	curl_setopt ($clSession, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt ($clSession, CURLOPT_HEADER, TRUE);
 	curl_setopt ($clSession, CURLOPT_NOBODY, TRUE);
-	
+
 	$sScrape = curl_exec($clSession);
 	curl_close($clSession);
-	
+
 	$sPingbackAddress = trim(TextBetween("<title>", "</title>", $sScrape));
-	
+
 	return $sPingbackAddress;
 }
 
@@ -212,20 +212,20 @@ function getTitleByUrl($sURL) {
 */
 function GetXPingback($sURL) {
 	$clSession = curl_init();
-	$iTimeout = 30; 
-	
-	curl_setopt ($clSession, CURLOPT_URL, $sURL); 
+	$iTimeout = 30;
+
+	curl_setopt ($clSession, CURLOPT_URL, $sURL);
 	curl_setopt ($clSession, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt ($clSession, CURLOPT_CONNECTTIMEOUT, $iTimeout);
 	curl_setopt ($clSession, CURLOPT_FOLLOWLOCATION, TRUE);
 	curl_setopt ($clSession, CURLOPT_HEADER, TRUE);
 	curl_setopt ($clSession, CURLOPT_NOBODY, TRUE);
-	
+
 	$sScrape = curl_exec($clSession);
 	curl_close($clSession);
-	
+
 	$sPingbackAddress = trim(TextBetween("X-Pingback: ", "n", $sScrape));
-	
+
 	return $sPingbackAddress;
 }
 
@@ -237,7 +237,7 @@ function GetXPingback($sURL) {
  * @param 	string 	$s2
  * @param 	string 	$s3
  * @return 	string	text
-*/	
+*/
 function TextBetween($s1,$s2,$s){
 	$s1 = mb_strtolower($s1);
 	$s2 = mb_strtolower($s2);
@@ -250,14 +250,14 @@ function TextBetween($s1,$s2,$s){
 	}
 	if($pos1 !== false){
 		if($s2 == '') return substr($s,$pos1+$L1);
-		
+
 		$pos2 = strpos(substr($scheck,$pos1+$L1),$s2);
 		if($pos2!==false) return substr($s,$pos1+$L1,$pos2);
 	}
 	return '';
 }
-        
-        
+
+
 
 $a = array( "pingback.ping" => array( "function" => "pbprocess" ));
 $s = new xmlrpc_server($a, false);
