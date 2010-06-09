ab3c8cf0b2096037d5f772b0e4cc2bf1fb566f3a
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-06-09 10:09:57 +0000

    Added SQL index on IRRE columns for faster retrieval
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3blog/trunk@34053 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,12 +3,12 @@
 #
 #
 CREATE TABLE tx_t3blog_post_content_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
+uid_local int(11) DEFAULT '0' NOT NULL,
+uid_foreign int(11) DEFAULT '0' NOT NULL,
+tablenames varchar(30) DEFAULT '' NOT NULL,
+sorting int(11) DEFAULT '0' NOT NULL,
+KEY uid_local (uid_local),
+KEY uid_foreign (uid_foreign)
 );
 
 
@@ -17,12 +17,12 @@ CREATE TABLE tx_t3blog_post_content_mm (
 #
 #
 CREATE TABLE tx_t3blog_post_cat_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(30) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL,
-  KEY uid_local (uid_local),
-  KEY uid_foreign (uid_foreign)
+uid_local int(11) DEFAULT '0' NOT NULL,
+uid_foreign int(11) DEFAULT '0' NOT NULL,
+tablenames varchar(30) DEFAULT '' NOT NULL,
+sorting int(11) DEFAULT '0' NOT NULL,
+KEY uid_local (uid_local),
+KEY uid_foreign (uid_foreign)
 );
 
 
@@ -71,38 +71,38 @@ CREATE TABLE tx_t3blog_post (
 # Table structure for table 'tx_t3blog_blogroll'
 #
 CREATE TABLE tx_t3blog_blogroll (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
-    t3ver_oid int(11) DEFAULT '0' NOT NULL,
-    t3ver_id int(11) DEFAULT '0' NOT NULL,
-    t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-    t3ver_label varchar(30) DEFAULT '' NOT NULL,
-    t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-    t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
-    t3ver_count int(11) DEFAULT '0' NOT NULL,
-    t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-    t3_origuid int(11) DEFAULT '0' NOT NULL,
-    sys_language_uid int(11) DEFAULT '0' NOT NULL,
-    l18n_parent int(11) DEFAULT '0' NOT NULL,
-    l18n_diffsource mediumblob NOT NULL,
-    sorting int(10) DEFAULT '0' NOT NULL,
-    deleted tinyint(4) DEFAULT '0' NOT NULL,
-    hidden tinyint(4) DEFAULT '0' NOT NULL,
-    starttime int(11) DEFAULT '0' NOT NULL,
-    endtime int(11) DEFAULT '0' NOT NULL,
-    fe_group int(11) DEFAULT '0' NOT NULL,
-    title tinytext NOT NULL,
-    url tinytext NOT NULL,
-    image blob NOT NULL,
-    description text NOT NULL,
-    xfn varchar(20) DEFAULT '' NOT NULL,
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(30) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	fe_group int(11) DEFAULT '0' NOT NULL,
+	title tinytext NOT NULL,
+	url tinytext NOT NULL,
+	image blob NOT NULL,
+	description text NOT NULL,
+	xfn varchar(20) DEFAULT '' NOT NULL,
 
-    PRIMARY KEY (uid),
-    KEY parent (pid),
-    KEY t3ver_oid (t3ver_oid,t3ver_wsid)
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid)
 );
 
 
@@ -191,7 +191,7 @@ CREATE TABLE tx_t3blog_com_nl (
 	post_uid int(11) DEFAULT '0' NOT NULL,
 	lastsent int(11) DEFAULT '0' NOT NULL,
 	code tinytext NOT NULL,
-	
+
 	PRIMARY KEY (uid),
 	KEY parent (pid)
 );
@@ -201,23 +201,23 @@ CREATE TABLE tx_t3blog_com_nl (
 # Table structure for table 'tx_t3blog_pingback'
 #
 CREATE TABLE tx_t3blog_pingback (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
-    sorting int(10) DEFAULT '0' NOT NULL,
-    deleted tinyint(4) DEFAULT '0' NOT NULL,
-    hidden tinyint(4) DEFAULT '0' NOT NULL,
-    starttime int(11) DEFAULT '0' NOT NULL,
-    endtime int(11) DEFAULT '0' NOT NULL,
-    title tinytext NOT NULL,
-    url tinytext NOT NULL,
-    date int(11) DEFAULT '0' NOT NULL,
-    text text NOT NULL,
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	sorting int(10) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	starttime int(11) DEFAULT '0' NOT NULL,
+	endtime int(11) DEFAULT '0' NOT NULL,
+	title tinytext NOT NULL,
+	url tinytext NOT NULL,
+	date int(11) DEFAULT '0' NOT NULL,
+	text text NOT NULL,
 
-    PRIMARY KEY (uid),
-    KEY parent (pid)
+	PRIMARY KEY (uid),
+	KEY parent (pid)
 );
 
 
@@ -225,21 +225,21 @@ CREATE TABLE tx_t3blog_pingback (
 # Table structure for table 'tx_t3blog_trackback'
 #
 CREATE TABLE tx_t3blog_trackback (
-    uid int(11) NOT NULL auto_increment,
-    pid int(11) DEFAULT '0' NOT NULL,
-    tstamp int(11) DEFAULT '0' NOT NULL,
-    crdate int(11) DEFAULT '0' NOT NULL,
-    cruser_id int(11) DEFAULT '0' NOT NULL,
-    deleted tinyint(4) DEFAULT '0' NOT NULL,
-    hidden tinyint(4) DEFAULT '0' NOT NULL,
-    fromurl varchar(100) DEFAULT '' NOT NULL,
-    text varchar(255) DEFAULT '' NOT NULL,
-    title varchar(50) DEFAULT '' NOT NULL,
-    blogname varchar(100) DEFAULT '' NOT NULL,
-    postid blob NOT NULL,
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+	tstamp int(11) DEFAULT '0' NOT NULL,
+	crdate int(11) DEFAULT '0' NOT NULL,
+	cruser_id int(11) DEFAULT '0' NOT NULL,
+	deleted tinyint(4) DEFAULT '0' NOT NULL,
+	hidden tinyint(4) DEFAULT '0' NOT NULL,
+	fromurl varchar(100) DEFAULT '' NOT NULL,
+	text varchar(255) DEFAULT '' NOT NULL,
+	title varchar(50) DEFAULT '' NOT NULL,
+	blogname varchar(100) DEFAULT '' NOT NULL,
+	postid blob NOT NULL,
 
-    PRIMARY KEY (uid),
-    KEY parent (pid)
+	PRIMARY KEY (uid),
+	KEY parent (pid)
 );
 
 
@@ -255,5 +255,7 @@ CREATE TABLE be_users (
 #
 CREATE TABLE tt_content (
 	irre_parentid int(11) DEFAULT '0' NOT NULL,
-	irre_parenttable tinytext NOT NULL
+	irre_parenttable tinytext NOT NULL,
+
+	KEY t3blog(irre_parentid,irre_parenttable(10))
 );
